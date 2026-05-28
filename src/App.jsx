cea2091752683a81e4b8ff118b4044d9c279a5f1
The-Roadie-Bible <johnhaggart@ymail.com>
2026-05-28 18:52:55 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -723,7 +723,7 @@ async function uploadImage(file) {
             to help the rest of the community find the best places around.
           </p>
         </section>
-        <section id="search-section" className="max-w-7xl mx-auto px-4 mt-10 mb-50">
+        <section id="search-section" className="max-w-7xl mx-auto px-4 mt-10 mb-40">
   <div className="max-w-7xl mx-auto">
     <div className="grid sm:grid-cols-2 lg:grid-cols-5 gap-3">
 
